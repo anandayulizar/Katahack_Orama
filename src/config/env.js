f9efcb3961dc4d11a9fd4a91693aad53744e7663
Yahya1547 <1547yahya@gmail.com>
2020-11-16 14:53:25 +0700

    add chatbot dialogflow (dummy)

---
+++ src/config/env.js
@@ -0,0 +1,12 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "bikeshop-jjkd",
+    "private_key_id": "537ddca8c151635444b54b669628612e2ff369a3",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDBlXc5Ghrj15bH\nTbcWgodEEBpoTrcr+zvD56+2isUXi1AZdXYskUAqD/cZ1UuyK7IjvhT70bnNakr3\nR6172dfZ5i88BsYEvI0ktE//ZRI5+6cCt74fyd8v5W+bOZS96cPuF8UpAC1m/0NB\naExgxBNDA/hwOmq6ty02LKrS73YZUGYohInHTBiha5z3AJAqkcolFnFjV6Y/NFCh\no5qbf21Miyp93NovrcojSSkfU3HEfPixcS/Fjnm/3v8pBGZMD8YEqRFzYq36CymW\nsXS/4BbssOXHsHJi8LnjptwJDXC3Q/KvD1OZEDsHHOtlrbyMvMOiveFkDBVBHTp9\nQWKazP2RAgMBAAECggEAHFIFPeOZjP0giBpQv3iLmPPBJULkzciksvGddXdiQ6JE\nLuFOD1GVoUGyLQOVHiUB2/RGRhvl0o0rF/eSxAR37V8dSGsWQ6GhBX5imLK84qI0\nCfP/Gs/x+KQYeo1HOyxsl1O/apLl0YRlHrfHOr3csohta1/Edvnu4ZbRy+ev755f\n389ETxbPn8kCB8ypYk4n/F8VT0US1oI/pjMcbUstVbnAWsMZ+JnD5DxP4HtRYR0Z\ncYOAx6unDzUvMJn/2wfEn3Tac6/FBuwDUeX/1vbGTOmtrR7GpzJWwl4uGJjuCBgI\nnbYIfx/dGTAHb0rs6frn5LAPMbSjpbBzpSLO2zYb5QKBgQDyfrIXXGbliM1v54C0\nKzGCkq8CVQfoyP7eOLWfneLBjXZdp9ZIen8d47gTPmy2NR0YlhucvHIOit+DmD/c\ngky7XFiJgSG4IfDnzdy7ED0OD1gPU+PqB2YSudKORMUX1YFOhkinmO79MKFkCCs7\nMuTkgiwC4tfeh3uWDGBLbCZxpQKBgQDMXW8ky7IU/sSd3OqcVlNy4uzJKwtqrIW+\nwxPkYLBhF//DtfBw/nMS2/X8C10WXwABQLJbtrUpL76ppmdTvQW3SPylfVayGWlY\nQyAKsL+hd7szapTw6xyX/eo2cxd2HKPvvGFj83bOBvDjexx9VcNJ6Fv9f0d+4bu4\nVKT8xlGAfQKBgQCKZFgLEQcAVFS6WqqPKF93H3OVpyV+asdZ149/6sFwqOU2igS3\nFIHbcwyonuW4DxPt5i0YBEwrlIH/PjKkDBDylL9KqlGuHmC4SjqA38GYdpVRGhVm\niJZudXJ/EyxQb97+C5EHZTZvdQOGbUsO2/rZlpi7hO6h5zxW4/VI1g4SqQKBgCUv\nlyq12diSJwqmiRYbayHLgtRz/abSb5+vdW4n3dA9ArAE6hRVaHD1D0RmePC6JpET\nyaCAlWRCKMpL2h9VlLWuFTKzpNPRZebGiA0J7kSeObT4XtObpF9Pt49iz9EREw9r\nUrlSMrr4blqmKx/vPBK1sg9lWogO4Q6AcGkCWRAtAoGAG26+W2ZNA3Fig5G8qLDS\nPy0OiwAhqO8qBvPA4PRhuh3sLaCDocPP65z9o23d1lGIXWnVwF+8/VZUCgtt+YTT\nyrD5YBJSTRujLoJkGA25VXjpbQ0uxWRIbLmeUuVLCt8DZABUeHlPPdPOqGevvWfF\nLbj1QNq09YmTSbZ9QXvjo4Y=\n-----END PRIVATE KEY-----\n",
+    "client_email": "yahya-330@bikeshop-jjkd.iam.gserviceaccount.com",
+    "client_id": "105484077195056986480",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/yahya-330%40bikeshop-jjkd.iam.gserviceaccount.com"
+}
